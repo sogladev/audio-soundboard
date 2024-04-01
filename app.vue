f752419e1a8f8eefa72691d1ecc720e3c02f6ef7
Jelle Meeus <jellemeeus@hotmail.com>
2024-04-01 13:08:47 +0200

    feat: Add navbar

--- app.vue
+++ app.vue
@@ -3,6 +3,7 @@
   <div>
     <h1>hello world</h1>
     <div className="mt-12 flex flex-wrap justify-center">
+    <NavBar></NavBar>
       <SoundBox text="Left" id="left-audio" />
       <SoundBox text="Right" id="right-audio" />
     </div>

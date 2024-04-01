b8f746f646da0ec3f8590db89fa9a41d7872c6b5
Jelle Meeus <jellemeeus@hotmail.com>
2024-04-01 13:09:16 +0200

    feat: Add description box

--- app.vue
+++ app.vue
@@ -1,9 +1,11 @@
 
 <template>
   <div>
-    <h1>hello world</h1>
     <div className="mt-12 flex flex-wrap justify-center">
     <NavBar></NavBar>
+    <h1 class="description text-xl mx-0 my-4 p-4 border-2 text-center">
+      Click to test stereo audio
+    </h1>
       <SoundBox text="Left" id="left-audio" />
       <SoundBox text="Right" id="right-audio" />
     </div>

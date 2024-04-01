cad4becc7a3b42ada8aabdabf0aee36883765753
Jelle Meeus <jellemeeus@hotmail.com>
2024-04-01 12:04:26 +0200

    config: update baseURL, head, disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: false,
+  app: {
+    head: {
+      title: 'Nuxt3 - audio-soundboard',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'audio-soundboard' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+    },
+    baseURL: (process.env.NODE_ENV == "production") ? '/audio-soundboard/' : '/',
+  },
   modules: ["@nuxtjs/tailwindcss"],
 });

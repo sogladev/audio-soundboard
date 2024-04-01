dffbb02aa7bfc91a8e20879e0b7fa6c22bf949a1
Jelle Meeus <jellemeeus@hotmail.com>
2024-04-01 13:12:41 +0200

    style: fix spacing

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@
     <h1 class="description text-xl mx-0 my-4 p-4 border-2 text-center">
       Click to test stereo audio
     </h1>
-    <div class="mt-4 flex justify-between">
+    <div class="mt-4 flex justify-between space-x-4">
       <SoundBox text="Left" id="left-audio" />
       <SoundBox text="Right" id="right-audio" />
     </div>

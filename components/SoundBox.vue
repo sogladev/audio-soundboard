f7c98d63108cf1bea386bd0356ef452fd030640a
Jelle Meeus <jellemeeus@hotmail.com>
2024-04-01 12:10:20 +0200

    (WIP) add soundbox component

---
+++ components/SoundBox.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+defineProps({
+  text: String,
+  id: String,
+})
+</script>
+
+<template>
+    <div :id="id">
+        {{ text }}
+    </div>
+</template>
+
+<style scoped></style>

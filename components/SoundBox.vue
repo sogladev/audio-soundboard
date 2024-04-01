aef2523682977d05fdd1d3d4ea1a8dd8e333cf07
Jelle Meeus <jellemeeus@hotmail.com>
2024-04-01 12:37:28 +0200

    feat: playAudio correctly

--- components/SoundBox.vue
+++ components/SoundBox.vue
@@ -1,14 +1,38 @@
 <script lang="ts" setup>
-defineProps({
-  text: String,
-  id: String,
+const props = defineProps({
+    text: String,
+    id: String,
 })
+
+const isPlaying = ref(false);
+
+function onClick() {
+    if (props.id == undefined) {
+        return;
+    }
+    const track = document.getElementById(props.id) as HTMLAudioElement | null
+    if (!track) {
+        return;
+    }
+
+    track.currentTime = 0;
+
+    track.addEventListener("ended", () => {
+        isPlaying.value = false;
+        return;
+    }, { once: true });
+
+    track.play();
+    isPlaying.value = true;
+};
 </script>
 
 <template>
-    <div :id="id">
-        {{ text }}
+    <div className="mt-3">
+        <button @click="onClick"
+            class="bg-emerald-600 hover:bg-emerald-700 transition py-16 mr-3 px-14 w-64 rounded border-b-4"
+            :class="{ 'border-emerald-600': isPlaying }">
+            {{ text }}
+        </button>
     </div>
 </template>
-
-<style scoped></style>

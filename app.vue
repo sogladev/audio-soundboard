3bcbd688c224e1105417de5d7e3c9867733cfb04
Jelle Meeus <jellemeeus@hotmail.com>
2024-04-01 13:09:38 +0200

    style: fix class, justify-between

--- app.vue
+++ app.vue
@@ -1,11 +1,11 @@
 
 <template>
   <div>
-    <div className="mt-12 flex flex-wrap justify-center">
     <NavBar></NavBar>
     <h1 class="description text-xl mx-0 my-4 p-4 border-2 text-center">
       Click to test stereo audio
     </h1>
+    <div class="mt-4 flex justify-between">
       <SoundBox text="Left" id="left-audio" />
       <SoundBox text="Right" id="right-audio" />
     </div>

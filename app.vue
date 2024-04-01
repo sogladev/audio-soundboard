8b6ca8f450fe8407087f7a92a99c9b26aee1bba4
Jelle Meeus <jellemeeus@hotmail.com>
2024-04-01 12:33:33 +0200

    feat: audio component

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
+
+import AudioComponent from './components/AudioComponent.vue';
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>hello world</h1>
+    <div className="mt-12 flex flex-wrap justify-center">
+      <SoundBox text="Left" id="left-audio" />
+      <SoundBox text="Right" id="right-audio" />
+    </div>
+      <AudioComponent></AudioComponent>
   </div>
 </template>
---
+++ components/AudioComponent.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <audio src="./left.mp3" id="left-audio"></audio>
+        <audio src="./right.mp3" id="right-audio"></audio>
+    </div>
+</template>
